c435e8fc5b55acf1d897bc406c485724267dbade
Konstantin Savusia <savconstantine@gmail.com>
2023-03-06 08:52:49 +0600

    added pixi app provider as main render component

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppProvider } from '@inlet/react-pixi'
+import * as PIXI from 'pixi.js'
 import Root from './config/root'
 
 import './assets/scss/main.scss'
 
+const app = new PIXI.Application()
+
 const target = document.getElementById('root')
 
-ReactDOM.render(<Root />, target)
+ReactDOM.render(
+  <AppProvider value={app}>
+    <Root />
+  </AppProvider>,
+  target
+)
