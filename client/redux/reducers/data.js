b1ff7a2dfbc7fcbef7bb3bf8a9ef88d5187f396c
Konstantin Savusia <savconstantine@gmail.com>
2023-03-08 01:04:04 +0600

    records component

--- client/redux/reducers/data.js
+++ client/redux/reducers/data.js
@@ -3,6 +3,38 @@ const SET_CHAT = 'SET_CHAT'
 const SET_ENEMY = 'SET_ENEMY'
 
 const initialState = {
+  records: [
+    {
+      id: 1,
+      name: 'Miracle Levin',
+      record: '2:44',
+      rank: '1st'
+    },
+    {
+      id: 2,
+      name: 'Lindsey Bergson',
+      record: '2:49',
+      rank: '2nd'
+    },
+    {
+      id: 3,
+      name: 'Madelyn Levin',
+      record: '2:54',
+      rank: '3rd'
+    },
+    {
+      id: 4,
+      name: 'Leo Stanton',
+      record: '3:22',
+      rank: '4th'
+    },
+    {
+      id: 5,
+      name: 'Martin George',
+      record: '3:24',
+      rank: '5th'
+    }
+  ],
   players: [],
   chat: [],
   enemy: {
