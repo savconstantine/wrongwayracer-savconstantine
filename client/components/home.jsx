8798763fe10a0880e007853667603c940db96f66
Konstantin Savusia <savconstantine@gmail.com>
2023-03-07 21:16:10 +0600

    update some styles and display info scene view

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
-import { Box, Grid, Tab, Tabs, useMediaQuery, useTheme } from '@mui/material'
+import React from 'react'
+import { Box, Grid, useMediaQuery, useTheme } from '@mui/material'
 import GameSceneView from './game-scene-view'
+import InfoSceneView from './info-scene-view'
 
 const Home = () => {
-  const [value, setValue] = useState(0)
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
-
   const theme = useTheme()
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
   const isMediumScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'))
@@ -17,43 +13,43 @@ const Home = () => {
   console.log('isMediumScreen', isMediumScreen) // eslint-disable-line no-console
   console.log('isLargeScreen', isLargeScreen) // eslint-disable-line no-console
 
+  const mainBoxStyles = {
+    flexGrow: 1,
+    background:
+      'linear-gradient(234.36deg, rgba(12, 12, 76, 0.5) 2.69%, rgba(6, 6, 6, 0) 43.67%), radial-gradient(168.67% 168.67% at 48.89% 54.41%, rgba(78, 32, 130, 0.5) 0%, rgba(12, 12, 76, 0.5) 71.88%), #080817;',
+    py: '42px',
+    minHeight: '100vh',
+    width: '100vw'
+  }
+
+  const wrapperBoxStyles = {
+    flexGrow: 1,
+    maxWidth: '1122px',
+    mx: 'auto'
+  }
+
+  const gameSceneViewGridStyles = {
+    filter: 'drop-shadow(0px 4px 90px #542899)',
+    borderRadius: '20px',
+    background: 'linear-gradient(180deg, #3D1B74 0%, #9A78D0 77.08%)',
+    p: '1px'
+  }
+
   return (
-    <Box sx={{ flexGrow: 1 }} maxWidth={1120} mx="auto">
-      <Grid container spacing={2}>
-        <Grid item lg={12}>
-          <GameSceneView />
-        </Grid>
+    <Box sx={mainBoxStyles}>
+      <Box sx={wrapperBoxStyles}>
+        <Grid container spacing={2}>
+          <Grid item lg={12}>
+            <Box sx={gameSceneViewGridStyles}>
+              <GameSceneView />
+            </Box>
+          </Grid>
 
-        {/* Second column */}
-        <Grid item lg={12}>
-          <Grid container spacing={2}>
-            {/* Desktop layout with 3 columns */}
-            <Grid item xs={12} md={4}>
-              <div>tab 1 content</div>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              {/* Second column content goes here */}
-            </Grid>
-            <Grid item xs={12} md={4}>
-              {/* Third column content goes here */}
-            </Grid>
-
-            {/* Mobile layout with 1 column and 3 tabs */}
-            <Grid item xs={12} md={0}>
-              <Tabs value={value} onChange={handleChange} centered>
-                <Tab label="Tab 1" />
-                <Tab label="Tab 2" />
-                <Tab label="Tab 3" />
-              </Tabs>
-              <div>
-                {value === 0 && <div>tab 1 content</div>}
-                {value === 1 && <div>tab 2 content</div>}
-                {value === 2 && <div>tab 3 content</div>}
-              </div>
-            </Grid>
+          <Grid item lg={12}>
+            <InfoSceneView />
           </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   )
 }
